7f609a412284e60eaaa3dc420a1e9e5af7c3120b
TWiedRW <twiederich@icloud.com>
2022-09-14 18:27:47 -0500

    Updated first replication

--- experiment_design.R
+++ experiment_design.R
@@ -5,6 +5,8 @@
 # - for Type 3, randomize placement of first bar
 #       within first 5, new bar within second 5
 
+require(ggplot2)
+
 set.seed(21)
 
 percent_smaller <- 0.82
@@ -23,16 +25,27 @@ position_newbar <- match(x = newbar, table = ordered_placement)
 #Groups 
 groups <- rep(c('A', 'B'), each = 5)
 
-dat <- data.frame(grouping = groups, yval = ordered_placement)
+#Color Groups
+bar_colors <- rep('random', 10)
+bar_colors[position_target] <- 'target'
+bar_colors[position_newbar] <- 'newbar'
+
+bar_colors
+
+dat <- data.frame(grouping = groups, yval = ordered_placement,
+                  bar_colors = bar_colors)
+dat
 
 #Plot
-ggplot(dat, mapping = aes(x = grouping, y = yval)) +
+ggplot(dat, mapping = aes(x = grouping, y = yval, fill = bar_colors)) +
   geom_col(position = position_dodge2(padding = 0),
            color = 'black',
-           fill = 'grey80',
+           #fill = 'grey80',
            width = 3/4) +
   scale_x_discrete() +
   theme_minimal() +
   theme(axis.title = element_blank(),
         panel.grid = element_blank(),
-        axis.text.y = element_blank())
+        axis.text.y = element_blank(),
+        axis.text.x = element_text(size = 18),
+        legend.position = 'none')
