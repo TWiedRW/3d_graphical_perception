38fb70d44482f36ad2d652da452055334bfd002e
TWiedRW <twiederich@icloud.com>
2023-03-01 11:19:25 -0600

    Remove test case

--- experiment_interface/code/convert_stl.py
+++ experiment_interface/code/convert_stl.py
@@ -6,5 +6,3 @@ from stl import mesh
 def convert_stl(file, output_path):
   ascii_stl = mesh.Mesh.from_file(file)
   ascii_stl.save(output_path)
-
-convert_stl('stl_files/id-03-Type1-Rep01.stl', 'test.stl')
